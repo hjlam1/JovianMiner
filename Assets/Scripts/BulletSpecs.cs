2cea288ab9a3be0d2c283f6701f7478de18b8850
Henry Lam <henry.j.lam@gmail.com>
2016-04-23 22:10:58 -0400

    4/23/16 10:13pm

--- Assets/Scripts/BulletSpecs.cs
+++ Assets/Scripts/BulletSpecs.cs
@@ -4,6 +4,7 @@ using UnityEngine.Networking;
 
 public class BulletSpecs : NetworkBehaviour {
 
+	private float bulletSpeed = 100.0f;
 	//[SyncVar]
 	//private float movementSpeed = 1000.0f;
 	//public Vector3 firedFrom;
@@ -11,7 +12,7 @@ public class BulletSpecs : NetworkBehaviour {
 	// Use this for initialization
 	void Start () {
 		//firedFrom = this.transform.forward;
-		transform.GetComponent<Rigidbody>().AddForce (transform.forward * 100.0f);
+		transform.GetComponent<Rigidbody>().AddForce (transform.forward * bulletSpeed);
 		//transform.GetComponent<Rigidbody>().AddForce(
 	}
 	
